041fc7330bee832fe9d47ebf839b0b1ecac6a050
bill-oliver <99285964+bill-oliver@users.noreply.github.com>
2023-05-16 14:15:36 -0400

    Update sqltags.js
    This doesn't work!

--- sqltags.js
+++ sqltags.js
@@ -3,6 +3,70 @@
 var fs = require('fs');
 
 //
+//  tagPosts - function
+//  --------
+//
+//  Add classification tags to each post
+//
+function tagPosts( ){
+	var dbPublii = new sqlite3.Database('db.sqlite');  // Publii database  (make a copy!!!)
+	var sSQLSelectTags = "SELECT id, name FROM tags WHERE name > '1.00' AND name < '99.99' ORDER BY name;";
+	var sSQLSelectPosts = "SELECT id, title FROM posts  WHERE slug BETWEEN ? AND ?;";
+	var sSQLAddPostID = "INSERT INTO posts_tags( tag_id, post_id ) VALUES( ?, ? );";
+	
+	//
+	//  find all the Classification tags
+	//
+	dbPublii.all( sSQLSelectTags,
+								[], ( err, rowsTag ) => {
+		if( err )
+			throw err;
+	  else{
+			// console.log( rowsTag );
+			var i;
+			for( i=0; i < rowsTag.length-1; i++ ){
+				//
+				//  For all posts with this tag
+				//
+				dbPublii.all(  sSQLSelectPosts,
+											[ rowsTag[i].name, rowsTag[i+1].name ],
+											( err, rowsPost ) => {
+					if( err )
+						throw err;
+					else{
+						//
+						//  Add them to the database
+						//
+						console.log( i, rowsTag[i].id, rowsTag[i].name );
+						console.log( rowsPost );
+						// console.log( i, rowsTag[i].id, rowsTag[i].name, rowPost.id, rowPost.title );
+						// dbPublii.run( sSQLAddPostID,
+						              // [ rowsTag[i].id, rowPost.id ] );
+					}
+				});
+			}
+				
+		//
+		//  Process the posts with the last tag
+		//
+		// dbPublii.each( sSQLSelectPosts,
+									// [ rowsTag[i].name, "99.99" ],
+									// ( err, rowPost ) => {
+				// if( err )
+					// throw err;
+				// else{
+					// console.log( rowsTag[rowsTag.length].id, rowsTag[rowsTag.length].name, rowPost.id, rowPost.title );
+					// dbPublii.run( sSQLAddPostID,
+												// [ rowsTag[rowsTag.length].id, rowPost.id ] );
+				// }
+			// });
+		}
+	});
+	
+	dbPublii.close();
+}
+	
+//
 //  createTags - function
 //  ----------
 //
@@ -51,7 +115,7 @@ function createTags( ){
 	});
 	
 	// dbPublii.close();
-	// dbReports.close();
+	// dbReports.close();   // **** vClosing too early!
 }
 	
 //
@@ -61,7 +125,8 @@ function createTags( ){
 const sqlite3 = require('sqlite3').verbose();
 
 try {
-	createTags();
+	// createTags();
+	tagPosts();
 }
 catch( e ) {
 	console.error( e.name );

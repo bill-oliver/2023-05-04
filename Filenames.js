e0f71280363c6718b9c84225abc0e86bff8513a9
bill-oliver <99285964+bill-oliver@users.noreply.github.com>
2023-05-21 15:04:53 -0400

    Fixed up image names

---
+++ Filenames.js
@@ -0,0 +1,39 @@
+//
+// Rename image files
+//
+var fs = require('fs');
+
+function fixImageFiles(){
+    let sPath = ".//images";
+
+    let ImageFiles = fs.readdirSync( sPath );
+    //console.log(ImageFiles);
+    
+    sPath += "//";
+
+    for( let i=0; i<ImageFiles.length; i++){
+
+        if( ImageFiles[i].toLowerCase().indexOf(".jpg") < 0 ){
+            throw new Error( ImageFiles[i] + "  is not a jpg********");
+        }
+
+        let iStrip = ImageFiles[i].indexOf(" (");
+        if( iStrip > 0 ){
+            let sNew = ImageFiles[i].slice( 0, iStrip-1 ) + ".jpg";
+//            console.log( ImageFiles[i], "->", sNew );
+            fs.renameSync( sPath + ImageFiles[i], sPath + sNew );
+        }
+    }
+}
+
+//
+//  Main
+//  ----
+//
+try {
+    fixImageFiles();
+}
+catch( e ) {
+	console.error( e.name );
+	console.error( e.message );
+}

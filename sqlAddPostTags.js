2a8a27637a2ccf88be1c726067e1922f2123e3aa
bill-oliver <99285964+bill-oliver@users.noreply.github.com>
2023-06-03 12:30:05 -0400

    Nothing done!

--- sqlAddPostTags.js
+++ sqlAddPostTags.js
@@ -6,11 +6,7 @@
 
 "use strict";
 
-const fs = require('fs');
 const sqlite3 = require('sqlite3').verbose();
-
-const sTagData1 = '{"viewConfig":{},"featuredImage":"';
-const sTagData2 = '","featuredImageAlt":"","featuredImageCaption":"","featuredImageCredits":"","isHidden":false,"metaTitle":"","metaDescription":"","metaRobots":"","canonicalUrl":"","template":""}';
 
 //
 //  CreateSlug - creates a valid slug from Classification code (can't have a dot)
@@ -33,70 +29,98 @@ function GetTag( sClassification ){
 }
 
 //
+//  WritePostTags - writes out the ordered list of tags
+//                   created by AddPostTags
+//
+//
+function WritePostTags( PostTags ){
+	var dbPublii = new sqlite3.Database('db.sqlite');
+	var sSQLPostTags = "INSERT INTO posts_tags( tag_id, post_id ) VALUES( ?, ? );";
+
+	console.log( "PostTags.length", PostTags.length );
+	for( let i = 0; i < PostTags.length; i += 2 ){
+		console.log( i, PostTags[i], PostTags[i+1] );
+		// dbPublii.run( sSQLPostTags, [ PostTags[i], PostTags[i+1] ] );
+	}
+
+	dbPublii.close();
+}
+
+//
 //  AddPostTags - Adds all the tags for a given report
 //
 //   sClassification - Classification code for report
 //
-function AddPostTags( sClassification ){
+function AddPostTags( ){
+	var dbReports = new sqlite3.Database('reports.sqlite')
 	var dbPublii = new sqlite3.Database('db.sqlite');
+
+	var sSQLClassification = "SELECT Classification FROM report;";
 	var sSQLPost = "SELECT id from posts WHERE slug = ?;";
 	var sSQLTag = "SELECT id from tags WHERE slug = ?;";
-	var sSQLPostTags = "INSERT INTO posts_tags( tag_id, post_id ) VALUES( ?, ? );";
 	
-	dbPublii.get( sSQLPost, [ sClassification ], (err, row) => {
-		if( err ){
-			throw new Error( "Post not found for " + sClassification );
-		}
+	var PostTags = [];				// Ordered list of TagId, PostIDs
 
-		var idPost = row.id;
-		var sTagSlug = CreateSlug( GetTag( sClassification ) );
-
-		dbPublii.get( sSQLTag, [ sTagSlug ], (err, row) => {
+	//
+	//  For each report in reports db, get its classification code
+	//
+	dbReports.each( sSQLClassification, [], (err, row) => {
+		//
+		//  Lookup the post in publii db
+		//
+		dbPublii.get( sSQLPost, [ row.Classification ], (err, row) => {
 			if( err ){
-				throw new Error( "Tag entry not found for " + sClassification );
+				throw new Error( "Post not found for " + sClassification );
 			}
 
+			var idPost = row.id;
+			var sTagSlug = CreateSlug( GetTag( sClassification ) );
 			//
-			// Now we have both IDs, link 'em
+			//  Lookup the tag for that post based on the classification code
 			//
-			var idTag = row.id;
-			console.log( sClassification, "tagID,PostID", idTag, idPost );
+			dbPublii.get( sSQLTag, [ sTagSlug ], (err, row) => {
+				if( err ){
+					throw new Error( "Tag entry not found for " + sClassification );
+				}
 
-			dbPublii.run( sSQLPostTags, [ idTag, idPost ] );
+				//
+				// Now we have both IDs, link 'em
+				//
+				var idTag = row.id;
+				PostTags.push( idTag );
+				PostTags.push( idPost);
+				// console.log( sClassification, "tagID,PostID", idTag, idPost );
+
+				// dbPublii.run( sSQLPostTags, [ idTag, idPost ] );
+			});
+		});
+	});
+
+	//
+	//  Must wait for the dbs to close to complete before we can access the Data
+	//
+	dbReports.close((err) => { 
+		if (err){
+			console.log(err.message);
+		}
+		dbPublii.close((err) => {
+			if (err){
+				console.log(err.message);
+			}
+			WritePostTags( PostTags );
 		});
 	});
 }
 
 //
-//  dbCallback - function
-//  ----------
-//
-//  Callback for Database.each function
-//
-function dbCallback( err, row ){
-	if( err ){
-		throw err;
-	}
-	setTimeout( () => {
-		AddPostTags( row.Classification );
-	} , 100 );
-}
-	
-//
 //  Main
 //  ----
 //
-let db = new sqlite3.Database('reports.sqlite')
-let sSQL = "SELECT Classification FROM report;"
-
 try {
-	// db.serialize();
-	db.each( sSQL, [], dbCallback );			// Process all items in db
+	AddPostTags();
 }
 catch( e ) {
 	console.error( e.name );
 	console.error( e.message );
 }
-
-db.close( );
 
